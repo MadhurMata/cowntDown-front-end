6849a1807df7cb521e4f1d9eb92943079a95ef4b
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-07-15 17:31:20 +0200

    fixed

--- src/routes-service.js
+++ src/routes-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class RoutesService {
   constructor() {
     this.user = axios.create({
-      baseURL: process.env.REACT_APP_API_URL,
+      baseURL: process.env.REACT_APP_BASE_URL,
       withCredentials: true
     })
   }
