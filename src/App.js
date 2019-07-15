05d703a2dabc6c4a87e449537e24612095f3f80f
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-07-15 11:12:02 +0200

    cowntdown done

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ export default class App extends Component {
     deadline: '2019-08-12',
     counter: {},
     names: [],
+    random: Math.floor(Math.random()*100),
+    colors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"],
   };
+  myParagraph = [];
 
   componentDidMount(){
-    this.getNames();
     setInterval(() => {
-      const counter = this.getTimeRemaining(this.state.deadline)
-        this.setState({counter})    
+      const counter = this.getTimeRemaining(this.state.deadline)     
+        this.getNames();
+        this.setState({
+          counter,
+        })    
     }, 1000);
+    setInterval(() => {
+      this.myParagraph.forEach((item) =>  {
+        item.classList.toggle('rotate');
+      });
+    }, 5000);
   }
 
   getNames = () => {
@@ -31,14 +41,6 @@ export default class App extends Component {
         console.log("error", error);
       });
   };
-
-  // didcomponentupdate(){
-    
-  //  const counter = this.getTimeRemaining(this.state.deadline)
-  //   if (this.state.counter === counter){
-  //     this.setState({counter})
-  //   }   
-  // } 
 
   handleChange = event => {
     let { name, value } = event.target;
@@ -54,8 +56,6 @@ export default class App extends Component {
       })
       .catch(error => console.log(error.response));
   };
-
-
 
 getTimeRemaining = (endTime) => {
   const timeDiference = Date.parse(endTime) - Date.parse(new Date());
@@ -73,54 +73,110 @@ getTimeRemaining = (endTime) => {
   return counter
 };
 
-  // getUser = (event) => {
-  //   event.preventDefault();
-  //   const { name } = this.state;
-  //   routeService.getUser(name.toLocaleLowerCase())
-  //     .then(data => {
-  //       if (data.length === 0){
-  //         this.setState({
-  //           route: [],
-  //           stage: 0,
-  //           message: true
-  //         })
-  //       } else {
-  //         this.setState({
-  //           route: data,
-  //           state: 1,
-  //           message: false
-  //         })
-  //       }
-  //     }).catch(error => { console.log("error", error)});
-  //   };
+  addSpanRef = (node) => {
+    this.myParagraph = [...this.myParagraph, node]
+  }
 
+  randomNumber = () =>{
+    const number = Math.floor(Math.random()*100)
+    const sign = Math.floor(Math.random()*10)
+    if(sign < 2){
+    return number
+      } else {
+    return number * -1
+      }
+  } 
 
-  render() {
-    const { name, deadline, counter, names } = this.state;
+  randomMovement = () => {
+    const { names, colors } = this.state
+    const oneColor = colors[Math.floor(Math.random()*colors.length)]
+
+    const start = Math.floor(Math.random());
+
+    names.map((name, index)=>{
+      if(start === 1){
+        return (
+        <div className="beer rotate"
+        key={index} 
+        ref={this.addSpanRef} 
+        style={{ 
+          color: `${oneColor}`, 
+          position: "absolute", 
+          top:`${Math.floor(Math.random()*100)}vh`, 
+          left:`${Math.floor(Math.random()*100)}vw`, 
+          transform: `translateX(${this.randomNumber()}vw) 
+          translateY(${this.randomNumber()}vh) 
+          rotate(${Math.random()*360}deg)`
+        }} >
+          <p>{name.name}</p>     
+        </div>
+        )   
+      } else {
+        return (
+          <div className="beer rotate"
+          key={index} 
+          ref={this.addSpanRef} 
+          style={{ 
+            color: `${oneColor}`, 
+            position: "absolute", 
+            top:`${Math.floor(Math.random()*100)}vh`, 
+            left:`${Math.floor(Math.random()*100)}vw`, 
+            transform: `translateY(${this.randomNumber()}vw) 
+            translateX(${this.randomNumber()}vh) 
+            rotate(${Math.random()*-360}deg)`
+          }} >
+            <p>{name.name}</p>     
+          </div>
+          ) 
+      }
+    })
+  }
+
+  render = () => {
+    const { name, counter, names, random } = this.state;
     return (
       <div className="App">
         <header className="App-header">
+        <div>
+            <h1>Beer Time!!</h1>
+            <h3>Join Mad's</h3>
+        </div>
         <form className="form" onSubmit={this.handleFormSubmit}>
                 <div className="input-box">
                   <Input
-                    label="Name"
+                    label="Write your name here..."
                     name="name"
                     value={name}
                     onChange={this.handleChange}
   
                   />
                 </div >
-                <div className="buttons">
-                  <button className="button">Search Route</button>
-                </div>
               </form>       
         </header>
         <main>
           <div className="counter">
-            <p>{counter.days} Days {counter.hours} h {counter.minutes} min {counter.seconds} sec</p>
+            <h2>{counter.days} <span className="display-letters">Days</span>  {counter.hours} <span className="display-letters">h</span> {counter.minutes} <span className="display-letters">min</span> {counter.seconds} <span className="display-letters">sec</span></h2>
           </div>
-          <div class="names-list">
-            {names.map((name)=>{return <p class="element">{name.name}</p> })}
+          <div className="names-list element">
+            {this.randomMovement() || names.map((name, index)=>{
+              return (
+                <div className="beer rotate"
+                key={index} 
+                ref={this.addSpanRef} 
+                style={{ 
+                  // color: `${oneColor}`, 
+                  position: "absolute", 
+                  top:`${Math.floor(Math.random()*100)}vh`, 
+                  left:`${Math.floor(Math.random()*100)}vw`, 
+                  transform: `translateX(${this.randomNumber()}vw) 
+                  translateY(${this.randomNumber()}vh) 
+                  rotate(${Math.random()*360}deg)`
+                }} >
+                  <p>{name.name}</p>     
+                </div>
+                )
+              }
+            )}
           </div>
         </main>
       </div>
