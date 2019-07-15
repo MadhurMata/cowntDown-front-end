65f6a90e23ef754f3f9cb6e368fc54bc15e4c60b
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-07-15 18:06:55 +0200

    names map

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     name: "",
     deadline: '2019-08-13',
     counter: {},
-    names: ['Mad'],
+    names: [],
     random: Math.floor(Math.random()*100),
     colors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"],
   };
@@ -94,7 +94,7 @@ getTimeRemaining = (endTime) => {
     const start = Math.floor(Math.random());
     if(names.length) {
       console.log(names)
-      names.map((name, index)=>{
+      ['Mad'].map((name, index)=>{
         if(start === 1){
           return (
           <div className="beer rotate"
@@ -160,7 +160,7 @@ getTimeRemaining = (endTime) => {
             <h2>{counter.days} <span className="display-letters">Days</span>  {counter.hours} <span className="display-letters">h</span> {counter.minutes} <span className="display-letters">min</span> {counter.seconds} <span className="display-letters">sec</span></h2>
           </div>
           <div className="names-list element">
-            {this.randomMovement() || names.map((name, index)=>{
+            {this.randomMovement() || ['Mad'].map((name, index)=>{
               return (
                 <div className="beer rotate"
                 key={index} 
